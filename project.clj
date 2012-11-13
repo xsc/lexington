f9300839c8e43dd07fc1cd0a80a58f2ab6943767
Yannick Scherer <yannick.scherer@gmail.com>
2012-11-13 12:18:18 +0100

    Initial Lein Templates.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lexington "0.1.0-SNAPSHOT"
+  :description "A Clojure Lexer (and possibly Parser) Generator"
+  :url "http://github.com/xsc/lexington"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
