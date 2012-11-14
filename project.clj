d98dcf462344860dee7e5725d2334e3f1a776ae5
Yannick Scherer <yannick.scherer@gmail.com>
2012-11-14 14:32:47 +0100

    Reformatted project.clj

--- project.clj
+++ project.clj
@@ -3,9 +3,5 @@
   :url "http://github.com/xsc/lexington"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-    [org.clojure/clojure "1.4.0"]
-  ]
-  :plugins [
-    [lein-marginalia "0.7.1"]
-  ])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-marginalia "0.7.1"]])
