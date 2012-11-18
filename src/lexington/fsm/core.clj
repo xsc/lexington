2ee3e5bdb583f738e28a42dd0746227b6da49fbf
Yannick Scherer <yannick.scherer@gmail.com>
2012-11-18 01:46:11 +0100

    Added first Recognizer implementation.

--- src/lexington/fsm/core.clj
+++ src/lexington/fsm/core.clj
@@ -44,56 +44,6 @@
   [s]
   (::transitions s))
 
-;; ### FSM Generation
-;;
-
-(defn states->fsm 
-  "Generate an FSM map from a sequence of state maps (created by `state`)."
-  [states]
-  (let [initial-state (state-name (first states))
-        state-table (reduce (fn [table s]
-                              (-> table
-                                (assoc (state-name s) (state-transitions s))))
-                            {}
-                            states)
-        state-keys (keys state-table)]
-    (-> {}
-      (assoc ::initial-state initial-state)
-      (assoc ::states (vec state-keys))
-      (assoc ::transition-table state-table))))
-
-
-
-;; ### FSM Instances (stateful)
-
-(defn run
-  [fsm]
-  (let [initial-map { :state (::initial-state fsm) 
-                      :input nil }
-        current-state (atom initial-map)
-        transitions (::transition-table fsm)]
-    (letfn [(next-state [{:keys[state input] :as m} e]
-
-              (if-let [t (or (get-in transitions [state e])
-                             (get-in transitions [state (any)]))]
-                (-> m
-                  (assoc :state t)
-                  (assoc :input (concat input [e])))
-                m))]
-      (fn [e]
-        (cond (= e ::reset) (swap! current-state (fn [x] initial-map))
-              :else (swap! current-state next-state e))))))
-
-
-;;(defmacro fsm
-;;  [& states]
-;;  (let [state-maps (vec (filter (comp not nil?)
-;;                                (map (fn [s]
-;;                                       (eval `(state ~@(rest s)))) states)))]
-;;    `(let [s# ~state-maps]
-;;       s#)))
-
-
 ;; ### Special Destination States
 
 (def continue
@@ -113,6 +63,11 @@
   (constantly ::accept))
 
 ;; ### Special Transition Rules
+
+(def eof
+  "Evaluates to `:lexington.fsm.core/eof`. Will initiate a transition on end-of-
+   data."
+  (constantly ::eof))
 
 (def any
   "Syntactic Sugar. Evaluates to `:lexington.fsm.core/any`. Will initiate a transition
@@ -136,3 +91,83 @@
   (when (seq args)
     (fn [s]
       (map #(vector % s) args))))
+
+;; ### FSM Generation
+
+(defn- handle-special-states
+  [state transitions]
+  (reduce 
+    (fn [t [k v]]
+      (let [v (cond (= v (continue)) state
+                    :else v)]
+        (assoc t k v)))
+    {}
+    transitions))
+
+(defn- generate-transition-table
+  [states]
+  (reduce (fn [table s]
+            (let [nm (state-name s)]
+              (if (get table nm)
+                (throw (IllegalArgumentException. (str "Duplicate State:" nm)))
+                (->> (handle-special-states nm (state-transitions s))
+                  (assoc table nm)))))
+          {}
+          states))
+
+(defn states->fsm 
+  "Generate an FSM map from a sequence of state maps (created by `state`)."
+  [states]
+  (let [initial-state (state-name (first states))
+        transitions (generate-transition-table states)
+        state-keys (keys transitions)]
+    (-> {}
+      (assoc :initial     initial-state)
+      (assoc :states      (set state-keys))
+      (assoc :transitions transitions))))
+
+;; ## FSM Instantiation
+
+;; ### Recognizer (stateless)
+;; A "stateless" FSM will run until one of the final states (accept/ignore-and-accept/reject)
+;; is reached. It is a function that takes an input and returns the number of input elements
+;; it recognized or nil.
+
+(defn recognizer
+  [{:keys [initial transitions] :as fsm}]
+  (letfn [(next-state [current-state input]
+            (if-let [new-state (or (get-in transitions [current-state input])
+                                   (get-in transitions [current-state (any)]))]
+              new-state
+              (reject)))]
+    (let [has-accepted? #{(accept) (ignore-and-accept)}
+          has-rejected? #{(reject)}]
+        (fn [in-seq]
+          (loop [state initial
+                 sq    in-seq
+                 c     0]
+            (cond (has-accepted? state) c
+                  (has-rejected? state) nil
+                  (not (seq sq)) (when (has-accepted? (next-state state (eof))) c)
+                  :else (when-let [s (next-state state (first sq))]
+                          (recur s (rest sq) (if (= s ::ignore-and-accept) c (inc c))))))))))
+
+;; ### FSM Instances (stateful)
+
+(defn run
+  [fsm]
+  (let [initial-map { :state (:initial-state fsm) 
+                      :input nil }
+        current-state (atom initial-map)
+        transitions (:transition-table fsm)]
+    (letfn [(next-state [{:keys[state input] :as m} e]
+
+              (if-let [t (or (get-in transitions [state e])
+                             (get-in transitions [state (any)]))]
+                (-> m
+                  (assoc :state t)
+                  (assoc :input (concat input [e])))
+                m))]
+      (fn [e]
+        (cond (= e ::reset) (swap! current-state (fn [x] initial-map))
+              :else (swap! current-state next-state e))))))
