ec29f940b7b0d06ce1d9671d7e93329080a64b89
Yannick Scherer <yannick.scherer@gmail.com>
2012-11-20 15:47:30 +0100

    Adapted FSM test to new generator.

--- test/lexington/fsm/fsm_tests.clj
+++ test/lexington/fsm/fsm_tests.clj
@@ -7,14 +7,20 @@
 
 (deftest fsm-structure
   (testing "Simple FSM Structure"
-    (let [fsm (states->fsm [ (state :l \l :i)
-                             (state :i \i :s)
-                             (state :s \s :p)
-                             (state :p 
-                                \p    :l
-                                (eof) (accept)) ])]
-      (is (= (:initial fsm) :l))
-      (are [s e n] (= (get-in (:transitions fsm) [s e]) n)
+    (let [f (fsm 
+              (state :l \l :i (eof) (accept))
+              (state :i \i :s)
+              (state :s \s :p)
+              (state :p \p :l))
+          transitions (:transitions f)]
+      (is (= (:initial f) :l))
+      (is (contains? (:accept f) (get-in transitions [:l (eof)])))
+      (are [s] (contains? (:reject f) (get-in transitions [s (any)]))
+           :l 
+           :i 
+           :s 
+           :p)
+      (are [s e n] (= (get-in transitions [s e]) n)
            :l \l :i
            :i \i :s
            :s \s :p
