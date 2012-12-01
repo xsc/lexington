8d162ab5f80701b73e3a664daa2b84eb4b63aaf2
Yannick Scherer <yannick.scherer@gmail.com>
2012-12-01 15:00:03 +0100

    Added FSM intersection/union/difference functions; examinination of FSM for unreachable states.

--- src/lexington/fsm/fsm.clj
+++ src/lexington/fsm/fsm.clj
@@ -13,6 +13,7 @@
 ;; - `:accept`      : a set of all states that let the FSM accept the input (if applicable)
 ;; - `:reject`      : a set of all states that let the FSM reject the input (if applicable)
 ;; - `:transitions` : a nested hash map `{ :current-state { <input> :next-state } }`
+;; - `:meta`        : a nested hash map `{ :key { :state <data> } ... }` for actions, ...
 ;;
 ;; Lexington takes a list of separate state maps (containing implicit references like 
 ;; `(continue)` or `(accept)`) and transforms it into the above representation, producing 
@@ -37,6 +38,7 @@
        (assoc :states (set (into states state-set)))
        (assoc :accept (set (into accept accept*)))
        (assoc :reject (set (into reject reject*)))
+       (assoc :meta {})
        (assoc :transitions (merge transitions transitions*))))))
 
 (defn ->fsm
@@ -155,3 +157,134 @@
   "Perform normalization on a list of states."
   [states]
   (mapcat normalize-single-state states))
+
+;; ## FSM Minimization
+
+(defn reachable-states
+  "Get a set of all reachable states based on an initial state and a transition map."
+  [root transitions]
+  (loop [reached-states #{root}
+         check-states [root]]
+    (let [new-states (mapcat
+                       (fn [s]
+                         (filter 
+                           (comp not reached-states)
+                           (vals (transitions s))))
+                       check-states)]
+      (if-not (seq new-states)
+        reached-states
+        (recur (set (concat reached-states new-states))
+               new-states)))))
+
+(defn unreachable-states
+  "Get a set of all unreachable states based on a set of all states, an initial one and
+   a transition map."
+  [states root transitions]
+  (let [reachable (reachable-states root transitions)]
+    (set (filter (comp not reachable) states))))
+
+(defn remove-unreachable-states
+  "Remove all unreachable states from an FSM."
+  [{:keys[initial transitions accept reject states] :as fsm}]
+  (let [reachable? (reachable-states initial transitions)]
+    (-> fsm
+      (assoc :accept (set (filter reachable? accept)))
+      (assoc :reject (set (filter reachable? reject)))
+      (assoc :states (set (filter reachable? states)))
+      (assoc :transitions (reduce (fn [m [s t]]
+                                    (if (reachable? s)
+                                      (assoc m s t)
+                                      m))
+                                  {}
+                                  transitions)))))
+
+(defn minimize
+  [fsm]
+  (-> fsm
+    remove-unreachable-states))
+
+;; ## FSM Transformation
+
+(defn invert
+  "Create an FSM that does accept everything not accepted by the given FSM.
+   This is done by setting all accepting states to rejecting ones and all other
+   states to accepting ones."
+  [{:keys[accept reject states] :as fsm}]
+  (-> fsm
+    (assoc :accept (set (filter (comp not accept) states)))
+    (assoc :reject accept)))
+
+(defn- cartesian-product-transitions
+  "Create transition map for cartesian product FSM."
+  [state-map t1 t2]
+  (into {}
+    (for [[x y :as s] (keys state-map)]
+      (let [tx (t1 x)
+            ty (t2 y)
+            in (set (concat (keys tx) (keys ty)))]
+        (when (and tx ty)
+          (vector (state-map s) 
+                  (into {}
+                    (for [e in] 
+                      (vector e (state-map [(tx e) (ty e)]))))))))))
+
+(defn cartesian-product
+  "Build the cartesian product of two FSM's. This is done by using the following data:
+- States: `(:states fsm1)` X `(:states fsm2)`
+- Initial State: (`(:initial fsm1)`, `(:initial fsm2)`)
+- Transitions: { [x, y] { <input> [(get-in fsm1 [:transitions x <input>]), ...] ... } ... }`
+  "
+  [fsm1 fsm2 accept? reject?]
+
+  ;; New states and unique names for them
+  (let [{ s1 :states i1 :initial t1 :transitions a1 :accept r1 :reject } fsm1
+        { s2 :states i2 :initial t2 :transitions a2 :accept r2 :reject } fsm2]
+    (let [states (for [x s1 y s2] [x y])
+          state-map (zipmap states (for [i (range)] (keyword (str "state-" i))))
+          initial (state-map [i1 i2])
+          accept (map state-map
+                      (filter (fn [[x y]]
+                                (accept? x y a1 a2))
+                              states))
+          reject (map state-map
+                      (filter (fn [[x y]]
+                                (reject? x y r1 r2))
+                              states))
+          transitions (cartesian-product-transitions state-map t1 t2)]
+      (minimize
+        (-> {}
+          (assoc :states (set (vals state-map)))
+          (assoc :initial initial)
+          (assoc :transitions transitions)
+          (assoc :accept (set accept))
+          (assoc :reject (set reject)))))))
+
+(defn intersect
+  "Create (cartesian product) intersection of two FSMs."
+  [fsm1 fsm2]
+  (cartesian-product 
+    fsm1 fsm2
+    (fn [x y a1 a2]
+      (and (a1 x) (a2 y)))
+    (fn [x y r1 r2]
+      (and (r1 x) (r2 y)))))
+
+(defn union
+  "Create (cartesian product) union of two FSMs."
+  [fsm1 fsm2]
+  (cartesian-product
+    fsm1 fsm2
+    (fn [x y a1 a2]
+      (or (a1 x) (a2 y)))
+    (fn [x y r1 r2]
+      (and (r1 x) (r2 y)))))
+
+(defn difference
+  "Create (cartesian product) difference of two FSMs."
+  [fsm1 fsm2]
+  (cartesian-product
+    fsm1 fsm2
+    (fn [x y a1 a2]
+      (and (a1 x) (not (a2 x))))
+    (fn [x y r1 r2]
+      (and (r1 x) (not (r2 y))))))
