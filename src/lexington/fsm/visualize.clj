efabb6577bac02eb1497aef246a0986b0da8f598
Yannick Scherer <yannick.scherer@gmail.com>
2012-12-02 16:17:29 +0100

    Visualization is now from left to right.

--- src/lexington/fsm/visualize.clj
+++ src/lexington/fsm/visualize.clj
@@ -2,42 +2,53 @@
        :author "Yannick Scherer" }
   lexington.fsm.visualize
   (:use dorothy.core
+        [clojure.string :as string :only [join]]
         [lexington.fsm.transitions :as t]
         [lexington.fsm.states :as s]
         lexington.fsm.transform))
+
+(defn- collect-transitions
+  "Convert a map of `{ <input> :next-state ... }` to `{ :next-state [<input> ...] ... }`"
+  [transitions]
+  (reduce
+    (fn [tr [e to]]
+      (merge-with concat
+        tr
+        { to [e] }))
+    {}
+    transitions))
 
 (defn fsm->dot
   "Convert FSM to GraphViz Dot format."
   [fsm]
   (let [{:keys[accept reject states transitions initial]} (remove-unreachable-states fsm)]
-    (letfn [(node-id [s]
-              (keyword (str "state-" (name s))))
-            (edge-id [from to]
-              (keyword (str "edge-" (name from) "-" (name to))))]
-      (-> 
-        (graph 
-          (concat
-            [(node-attrs { :shape :ellipse :height "0.8" :width "0.8" :fontsize "8pt" :fixedsize "true" })
-             (edge-attrs { :fontsize "8pt" })
-             [:__init { :style "invisible" :height "0.1" :width "0.1" }]]
+    (-> 
+      (graph 
+        (concat
+          [(graph-attrs { :rankdir "LR" })
+           (node-attrs { :shape :ellipse :height "0.8" :width "0.8" :fontsize "8pt" :fixedsize "true" })
+           (edge-attrs { :fontsize "8pt" })
+           [:__init { :style "invisible" :height "0.1" :width "0.1" }]]
 
-            (map (fn [s]
-                   (cond (accept s)
-                           [s { :color "darkgreen" :fontcolor "darkgreen" :style "bold" }]
-                         (reject s)
-                           [s { :color "red" :fontcolor "red" :style "bold" }]
-                         :else s))
-                 (filter #(not (= % s/reject!)) states))
-            [[:__init initial { :dir :forward }]]
-            (mapcat
-              (fn [[from to-map]]
+          (map (fn [s]
+                 (cond (accept s)
+                       [s { :color "darkgreen" :fontcolor "darkgreen" :style "bold" }]
+                       (reject s)
+                       [s { :color "red" :fontcolor "red" :style "bold" }]
+                       :else s))
+               (filter #(not (= % s/reject!)) states))
+          [[:__init initial { :dir :forward }]]
+          (mapcat
+            (fn [[from to-map]]
+              (let [ct (collect-transitions to-map)]
                 (filter (comp not nil?)
-                        (map (fn [[e to]]
+                        (map (fn [[to es]]
                                (when-not (= to s/reject!)
-                                 [from to { :dir :forward 
-                                           :label (if (= e t/any) "*" (str e)) }])) to-map)))
-              transitions)))
-        dot))))
+                                 (let [label (string/join "," (map #(if (= % t/any) "*" (str %)) es))]
+                                   [from to { :dir :forward :label label }])))
+                             ct))))
+            transitions)))
+      dot)))
 
 (defn show-fsm!
   "Open Dorothy Window to display the FSM."
